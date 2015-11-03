de6480e96cc2b5069a8fc8b602d3e94850413f86
DrScott <dougscott2@gmail.com>
2015-11-03 12:10:35 -0500

    implemented databases

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -79,11 +79,11 @@ public class Main {
         createTables(conn);
 
 
-	    HashMap<String, User> users = new HashMap();
+	   /* HashMap<String, User> users = new HashMap();
         ArrayList<Message> messages = new ArrayList();
 
         addTestUsers(users);
-        addTestMessages(messages);
+        addTestMessages(messages); */
 
         Spark.get(
                 "/",
@@ -91,12 +91,12 @@ public class Main {
                     Session session = request.session();
                     String username = session.attribute("username");
 
-                    ArrayList<Message> threads = new ArrayList();
-                    for (Message message : messages) {
+                    ArrayList<Message> threads = selectReplies(conn, -1);
+                  /*  for (Message message : messages) {
                         if (message.replyId == -1) {
                             threads.add(message);
                         }
-                    }
+                    } */
 
                     HashMap m = new HashMap();
                     m.put("threads", threads);
@@ -118,16 +118,16 @@ public class Main {
                     String id = request.queryParams("id");
                     try {
                         int idNum = Integer.valueOf(id);
-                        Message message = messages.get(idNum);
+                        Message message = selectMessage(conn, idNum);
                         m.put("message", message);
                         m.put("replyId", message.id);
 
-                        ArrayList<Message> replies = new ArrayList();
-                        for (Message msg : messages) {
+                        ArrayList<Message> replies = selectReplies(conn, message.id);
+                       /* for (Message msg : messages) {
                             if (msg.replyId == message.id) {
                                 replies.add(msg);
                             }
-                        }
+                        }*/
                         m.put("replies", replies);
                     } catch (Exception e) {
 
@@ -147,11 +147,12 @@ public class Main {
                         Spark.halt(403);
                     }
 
-                    User user = users.get(username);
+                    User user = selectUser(conn, username);
                     if (user == null) {
-                        user = new User();
+                        insertUser(conn, username, password);
+                        /*user = new User();
                         user.password = password;
-                        users.put(username, user);
+                        users.put(username, user);*/
                     }
                     else if (!password.equals(user.password)) {
                         Spark.halt(403);
@@ -178,8 +179,10 @@ public class Main {
                     String text = request.queryParams("text");
                     try {
                         int replyIdNum = Integer.valueOf(replyId);
-                        Message message = new Message(messages.size(), replyIdNum, username, text);
-                        messages.add(message);
+                        User me = selectUser(conn, username);
+                        insertMessage(conn, me.id, replyIdNum, text);
+                       // Message message = new Message(messages.size(), replyIdNum, username, text);
+                        //messages.add(message);
                     } catch (Exception e) {
 
                     }
